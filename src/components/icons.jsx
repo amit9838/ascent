60cfb437848b57b8691fc4adc775d157474a14e4
Amit Chaudhary <amitchaudhary0539@outlook.com>
2026-09-21 05:13:22 +0530

    feat: add ChartIcon

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -343,6 +343,16 @@ export function MountainIcon({ className }) {
   );
 }
 
+export function ChartIcon({ className }) {
+  return (
+    <Svg className={className}>
+      <line x1="12" x2="12" y1="20" y2="10" />
+      <line x1="18" x2="18" y1="20" y2="4" />
+      <line x1="6" x2="6" y1="20" y2="16" />
+    </Svg>
+  );
+}
+
 export function StarIcon({ className }) {
   return (
     <Svg className={className}>
